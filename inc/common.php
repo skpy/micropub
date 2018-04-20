c4c7de1776e7976476188c3064761376d2d29071
Scott Merrill <smerrill@covermymeds.com>
2018-04-19 21:25:25 -0400

    Update to support Micropub spec
    This has been tested against https://micropub.rocks/ and most
    functionality is implemented.

--- inc/common.php
+++ inc/common.php
@@ -3,6 +3,7 @@ function http_status ($code) {
     $http_codes = array(
         '200' => '200 OK',
         '201' => '201 Created',
+        '202' => '202 Accepted',
         '400' => '400 Bad Request',
         '401' => '401 Unauthorized',
         '403' => '403 Forbidden',
@@ -14,11 +15,16 @@ function http_status ($code) {
     return $http_codes[$code];
 }
 
-function quit ($code = 400, $error = '', $description = 'An error occurred.') {
+function quit ($code = 400, $error = '', $description = 'An error occurred.', $location = '') {
+    $code = (int) $code;
     $status = 'success';
     header("HTTP/1.1 " . http_status($code));
-    if ( int($code) >= 400 ) {
+    if ( $code >= 400 ) {
         echo json_encode(['error' => $error, 'error_description' => $description]);
+    } elseif ($code == 200 || $code == 201 || $code == 202) {
+        if (!empty($location)) {
+            header('Location: ' . $location);
+        }
     }
     die();
 }
@@ -26,6 +32,37 @@ function quit ($code = 400, $error = '', $description = 'An error occurred.') {
 function show_info() {
     echo '<p>This is a <a href="https://www.w3.org/TR/micropub/">micropub</a> endpoint.</p>';
     die();
+}
+
+function parse_request() {
+    if ( strtolower($_SERVER['CONTENT_TYPE']) == 'application/json' || strtolower($_SERVER['HTTP_CONTENT_TYPE']) == 'application/json' ) {
+        $request = \p3k\Micropub\Request::createFromJSONObject(json_decode(file_get_contents('php://input'), true));
+    } else {
+        $request = \p3k\Micropub\Request::createFromPostArray($_POST);
+    }
+    if($request->error) {
+        quit(400, $request->error_property, $request->error_description);
+    }
+    return $request;
+}
+
+/**
+ * getallheaders() replacement for nginx
+ *
+ * Replaces the getallheaders function which relies on Apache
+ *
+ * @return array incoming headers from _POST
+ */
+if (!function_exists('getallheaders')) {
+  function getallheaders() {
+    $headers = [];
+    foreach ($_SERVER as $name => $value) {
+      if (substr($name, 0, 5) == 'HTTP_') {
+        $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+      }
+    }
+    return $headers;
+  }
 }
 
 /**
@@ -57,8 +94,10 @@ function indieAuth($token, $me = '') {
         quit(401, 'insufficient_scope', 'The request lacks authentication credentials');
     } elseif ($response['me'] != $me) {
         quit(401, 'insufficient_scope', 'The request lacks valid authentication credentials');
-    } elseif (!in_array('create', $scopes) && !in_array('post', $scopes)) {
+    } elseif (is_array($response['scope']) && !in_array('create', $response['scope']) && !in_array('post', $response['scope'])) {
         quit(403, 'forbidden', 'Client does not have access to this resource');
+    } elseif (FALSE === stripos($response['scope'], 'create')) {
+        quit(403, 'Forbidden', 'Client does not have access to this resource');
     }
     // we got here, so all checks passed. return true.
     return true;
@@ -74,7 +113,7 @@ function show_config($show = 'all') {
         }
     }
 
-    $conf = array("media-endpoint" => 'https://' . $config['base_url'] . '/micropub/media.php');
+    $conf = array("media-endpoint" => $config['base_url'] . 'micropub/index.php');
     if ( ! empty($syndicate_to) ) {
         $conf['syndicate_to'] = $syndicate_to;
     }
@@ -88,17 +127,8 @@ function show_config($show = 'all') {
     exit;
 }
 
-# ensure that this battle station is fully operational.
-function check_target_dir($target_dir = '') {
- if ( empty($target_dir)) {
-    quit(400, 'unknown_dir', 'Unspecified directory');
-  }
-  # make sure our upload directory exists
-  if ( ! file_exists($target_dir) ) {
-    # fail if we can't create the directory
-    if ( FALSE === mkdir($target_dir, 0777, true) ) {
-      quit(400, 'cannot_mkdir', 'The upload directory could not be created.');
-    }
-  }
+function build_site() {
+    global $config;
+    exec( $config['command']);
 }
 ?>
--- index.php
+++ index.php
@@ -1,19 +1,35 @@
 <?php
-$plugins = array();
 
 # we can't do anything without a config file
 if ( ! file_exists('config.php') ) {
     die;
 }
 $config = include_once './config.php';
+date_default_timezone_set($config['tz']);
+
 # invoke the composer autoloader for our dependencies
 require_once __DIR__.'/vendor/autoload.php';
-use Symfony\Component\Yaml\Yaml;
 
 # load our common libraries
 include_once './inc/common.php';
 include_once './inc/content.php';
-#require_once './inc/plugin.php';
+include_once './inc/media.php';
+
+// Take headers and other incoming data
+$headers = getallheaders();
+if ($headers === false ) {
+    quit(400, 'invalid_headers', 'The request lacks valid headers');
+}
+$headers = array_change_key_case($headers, CASE_LOWER);
+if (!empty($_POST['access_token'])) {
+    $token = "Bearer ".$_POST['access_token'];
+    $headers["authorization"] = $token;
+}
+if (! isset($headers['authorization']) ) {
+    quit(401, 'no_auth', 'No authorization token supplied.');
+}
+// check the token for this connection.
+indieAuth($headers['authorization'], $config['base_url']);
 
 # is this a GET request?
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
@@ -23,7 +39,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
                 show_config();
                 break;
             case 'source':
-               show_content_source($_GET['url']);
+               show_content_source($_GET['url'], $_GET['properties']);
                 break;
             case 'syndicate-to':
                 show_config('syndicate-to');
@@ -38,21 +54,44 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (array_key_exists('file', $_FILES)) {
         # this appears to be a file upload. Handle it.
-        include_once('./inc/media.php');
-        media_upload();
+        #
+        # NOTE: micropub media uploads expect the file to be immediately
+        # available, so we upload to the site's directory, and optionally
+        # copy to the source directory for inclusion with later site builds.
+        # uploads will be stored at '/base/uploads/YYYY/mm/'
+        $subdir = date('Y/m/');
+        $upload_path = $config['base_path'] . $config['upload_path'] . $subdir;
+        $copy_path = $config['source_path'] . 'static/' . $config['upload_path'] . $subdir;
+        check_target_dir($upload_path);
+        $upload = media_upload($upload_path, $config['max_image_width']);
+        # do we need to copy this file to the source /static/ directory?
+        if ($config['copy_uploads_to_source'] === TRUE ) {
+            # we need to ensure '/source/statuc/uploads/YYYY/mm/' exists
+            check_target_dir($copy_path);
+            if ( copy ( $upload_path . $upload, $copy_path . $upload ) === FALSE ) {
+                quit(400, 'copy_error', 'Unable to copy upload to source directory');
+            }
+        }
+        $url = $config['base_url'] . $config['upload_path'] . $subdir . $upload;
+        header('HTTP/1.1 201 Created');
+        header('Location: ' . $url);
+        echo json_encode([ 'url' => $url ]); 
+        die();
     } else {
-        switch($_POST['action']):
+        # not a file upload. Parse the JSON or POST body into an object
+        $request = parse_request();
+        switch($request->action):
             case 'delete':
-                delete($_POST['url']);
+                delete($request);
                 break;
             case 'undelete':
-                undelete($_POST['url']);
+                undelete($request);
                 break;
             case 'update':
-                update($_POST['url']);
+                update($request);
                 break;
             default:
-                create();
+                create($request);
                 break;
         endswitch;
     }
